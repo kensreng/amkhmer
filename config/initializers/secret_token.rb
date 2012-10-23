c926ae4adb66f66f885f99e0fab0b4dfbaeb806e
kengsreng tang <kengsreng@yahoo.com>
2012-10-23 22:14:22 +0700

    added amkhmer project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Amkhmer::Application.config.secret_token = 'b68179fa309c40539dfb8016c9a251c9c3046a4ff1df63e33bbf53b32d85b0a440ad8fff4a6eb69b93c2a9d81261576eeb21ad0333600d2b9cb42e5eee08e8aa'
